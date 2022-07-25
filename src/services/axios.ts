6602fc54badbcfc42e226c52640983da29638e35
LuizEduardoVae <duduvivas18@hotmail.com>
2022-07-25 16:59:41 -0300

    vamooooo

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const Openmeteo = axios.create ({
-    baseURL: "https://api.open-meteo.com/v1/forecast?latitude=-20.26&longitude=-40.28&hourly=temperature_2m"
+    baseURL: "https://api.open-meteo.com/v1/forecast?"
 })
 
